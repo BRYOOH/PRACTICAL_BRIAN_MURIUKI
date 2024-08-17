68257187a005970b9f7e99828c9888458bed8c7c
BRYOOH <bryoohmuchira@gmail.com>
2024-08-17 22:42:50 +0300

    final

--- ASP.NET/obj/Debug/net8.0/ASP.NET.AssemblyInfo.cs
+++ ASP.NET/obj/Debug/net8.0/ASP.NET.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASP.NET")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bcd90ee5611c6553089b998623a136508148061")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+952fc7724afe450fce0fcdc6e8036adad663d87e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASP.NET")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASP.NET")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
